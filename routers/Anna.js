ddc3b0ca3f5e1f6210ea6180aef7c1d50ba4d503
Alivin1101 <sond455184@gmail.com>
2020-10-11 22:55:50 +0800

    change index.html

--- routers/Anna.js
+++ routers/Anna.js
@@ -14,7 +14,10 @@ router.get('/', function(req, res) {
 
 // define the about route
 router.get('/about', function(req, res) {
-  res.send('About Anna');
+  res.send(
+    '<h1> Hi,my name is Anna.</h1>'+
+    '\n <img src="https://www.google.com/url?sa=i&url=http%3A%2F%2Farxiv-ai.com%2Forgnization%3Fname%3DSenseTime%2520Research%2520%26%2520THU%2520%26%2520NTU&psig=AOvVaw0QHWFI-9yW44cqfYbWuxMD&ust=1602514023259000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCNCB5o3krOwCFQAAAAAdAAAAABAJ" height="70%">'
+    );
 });
 
 module.exports = router;
--- routers/Sam.js
+++ routers/Sam.js
@@ -14,7 +14,9 @@ router.get('/', function(req, res) {
 
 // define the about route
 router.get('/about', function(req, res) {
-  res.send('About Sam');
+  res.send(
+      '<h1> Hi,my name is Sam.</h1>'+
+  '\n <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fstore.line.me%2Fstickershop%2Fproduct%2F1008584%2Fzh-Hant&psig=AOvVaw27V_D1jeHC5OhlRsSip_sj&ust=1602514156164000&source=images&cd=vfe&ved=0CAIQjRxqFwoTCICm68vkrOwCFQAAAAAdAAAAABAD" height="70%">'
+  );
 });
-
 module.exports = router;
